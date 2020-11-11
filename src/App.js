1dd79c0be6fdd2ed6186b00481d180084dac4595
ilajosmanov <illiaosmanov@gmail.com>
2020-11-12 00:37:54 +0200

    added sources

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  }
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col my-2">
+          <button
+            className="btn btn-primary"
+            disabled={count <= 0}
+            onClick={decrement}
+          >
+            -
+          </button>
+          <span className="mx-5">
+            {count}
+          </span>
+          <button
+            className="btn btn-primary"
+            disabled={count >= 10}
+            onClick={increment}
+          >
+            +
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
