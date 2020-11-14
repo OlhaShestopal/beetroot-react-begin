b5f5b0d85642c9034991b9b89ce52b7f822d7256
ilajosmanov <illiaosmanov@gmail.com>
2020-11-14 12:26:37 +0200

    draft version App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,48 @@ import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState('');
 
   const increment = () => {
     if (count < 10) {
-      setCount(count + 1);
+      updateValue(count + 1);
     }
   }
 
   const decrement = () => {
     if (count > 0) {
-      setCount(count - 1);
+      updateValue(count - 1);
     }
   }
 
+  const updateValue = (n) => {
+    setCount(n);
+    setValue(n);
+  }
+
+  const handleChange = ({target}) => {
+    const n = +target.value;
+    setValue(target.value);
+
+    if (n < 0) {
+      setCount(0)
+      return;
+    }
+
+    if (n > 10) {
+      setCount(10)
+      return;
+    }
+
+    setCount(n);
+  }
+
+  const setErrorClass = n => (n < 0 || n > 10) ? 'is-invalid' : "";
+
   return (
     <div className="container">
-      <div className="row">
-        <div className="col my-2">
+      <div className="row my-2">
+        <div className="col-2 d-flex justify-content-center">
           <button
             className="btn btn-primary"
             disabled={count <= 0}
@@ -26,9 +51,17 @@ function App() {
           >
             -
           </button>
-          <span className="mx-5">
-            {count}
-          </span>
+        </div>
+
+        <div className="col d-flex justify-content-center">
+          <input
+            value={value}
+            className={`form-control ${setErrorClass(value)}`}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="col-2 d-flex justify-content-center">
           <button
             className="btn btn-primary"
             disabled={count >= 10}
